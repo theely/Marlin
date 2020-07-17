26a51b11e65114c4f2857912e64fbd5ec0ac8659
Elia Palme <elia.palme@gmail.com>
2020-07-17 17:00:51 +0200

    Update Configuration_adv.h

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2447,24 +2447,24 @@
    * STEALTHCHOP_(XY|Z|E) must be enabled to use HYBRID_THRESHOLD.
    * M913 X/Y/Z/E to live tune the setting
    */
-  //#define HYBRID_THRESHOLD
+  #define HYBRID_THRESHOLD
 
   #define X_HYBRID_THRESHOLD     100  // [mm/s]
   #define X2_HYBRID_THRESHOLD    100
   #define Y_HYBRID_THRESHOLD     100
   #define Y2_HYBRID_THRESHOLD    100
-  #define Z_HYBRID_THRESHOLD       3
-  #define Z2_HYBRID_THRESHOLD      3
-  #define Z3_HYBRID_THRESHOLD      3
-  #define Z4_HYBRID_THRESHOLD      3
-  #define E0_HYBRID_THRESHOLD     30
-  #define E1_HYBRID_THRESHOLD     30
-  #define E2_HYBRID_THRESHOLD     30
-  #define E3_HYBRID_THRESHOLD     30
-  #define E4_HYBRID_THRESHOLD     30
-  #define E5_HYBRID_THRESHOLD     30
-  #define E6_HYBRID_THRESHOLD     30
-  #define E7_HYBRID_THRESHOLD     30
+  #define Z_HYBRID_THRESHOLD       8
+  #define Z2_HYBRID_THRESHOLD      8
+  #define Z3_HYBRID_THRESHOLD      8
+  #define Z4_HYBRID_THRESHOLD      8
+  #define E0_HYBRID_THRESHOLD     50
+  #define E1_HYBRID_THRESHOLD     50
+  #define E2_HYBRID_THRESHOLD     50
+  #define E3_HYBRID_THRESHOLD     50
+  #define E4_HYBRID_THRESHOLD     50
+  #define E5_HYBRID_THRESHOLD     50
+  #define E6_HYBRID_THRESHOLD     50
+  #define E7_HYBRID_THRESHOLD     50
 
   /**
    * CoolStep. Currently supported for TMC2130, TMC2209, TMC5130 and TMC5160 only.
