fd62987658f7d3bf9a3f8263107f43990bf25ff5
Elia Palme <elia.palme@gmail.com>
2020-08-17 09:24:12 +0200

    Increase sqeze

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -988,7 +988,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -40, -12, -1.315 }
+#define NOZZLE_TO_PROBE_OFFSET { -40, -12, -1.35 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
