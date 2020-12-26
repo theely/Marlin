2cca00634d152d6b6f08c65657ea3d5bf629890c
Elia Palme <elia.palme@gmail.com>
2020-12-26 18:31:33 +0100

    Adjusted Nozzle offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -986,7 +986,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 32.4, -34.9, -0.63 }
+#define NOZZLE_TO_PROBE_OFFSET { 32.4, -34.9, -2.42 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -1131,7 +1131,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 220
+#define Z_MAX_POS 250
 
 /**
  * Software Endstops
